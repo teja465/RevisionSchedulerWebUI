68e45785803d15cc704f70cdf5de1ee58d61eb18
Raviteja Reddy Katukuri <tejaaa@amazon.com>
2022-12-04 20:56:22 +0530

    Add otp verification dueing signup"

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -17,15 +17,22 @@ function RegisterUser() {
     const [username, setusername] = useState("")
     const [password, setpassword] = useState("")
     const [confirmPassword, setconfirmPassword] = useState("")
-    const [nickName, setNickName] = useState("second") 
+    const [nickName, setNickName] = useState() 
     const [formValidationFailedMessage, setformValidationFailedMessage] = useState("")
     const [isValidationFailed, setisValidationFailed] = useState(false)
     const [isRegisterBtnVisisble, setisRegisterBtnVisisble] = useState(true)
     const [registerBtnText, setregisterBtnText] = useState("Register")
+    const [isRegisterBtnEnabled, setisRegisterBtnEnabled] = useState(true)
+
+    const [enableOTPTextbox, setenableOTPTextbox] = useState(false)
+    const [enableOtpValidateBtn, setenableOtpValidateBtn] = useState(false)
+    const [otp, setOtp] = useState("")
 
     const [alertType, setalertType] = useState("success")
     const [IsAlertEnabled, setIsAlertEnabled] = useState(false)
     const [AlertText, setAlertText] = useState("")
+
+    
 
     const handleFormValidationFailure=(errorMessage)=>{
         // console.log("in handleFormValidationFailure",errorMessage)
@@ -52,9 +59,12 @@ function RegisterUser() {
             headers:headers
         }).then(data=>{
             if(data.status == 201){
-                setAlertText("user created successfully .please login")
+                setAlertText("Sent otp via email , please submit otp  below")
                 setIsAlertEnabled(true)
                 setalertType("success")
+                setenableOTPTextbox(true)
+                setisRegisterBtnVisisble(false)
+                setisRegisterBtnEnabled(false)
             }
         }).catch(err=>{
             if (data.status=422){
@@ -95,6 +105,32 @@ function RegisterUser() {
                 setalertType("danger")
         }
     }
+    const handleValidateOTPclick=()=>{
+        console.log("otp is ",otp);
+        const headers= {
+            "content-type": "application/json",
+          }
+        const params={
+            "userEmail":username,
+            "otp":otp
+        }
+        const requestType='post';
+        const endpoint =configs[stage]['endpoint']+'/api/validate-user'
+        console.log("Calling ",endpoint+'/api/validate-user')
+        axios.post(endpoint,null,{params})
+        .then(data=>{
+            console.log(data)
+            setAlertText(data.data+" Please login")
+            setIsAlertEnabled(true)
+            setalertType("success")
+        })
+        .catch(err=>{
+            console.log(err)
+            setAlertText(err.response.data)
+            setIsAlertEnabled(true)
+            setalertType("danger")
+        })
+    }
   return (
     <MDBContainer className="my-5 d-flex flex-column col-12 col-lg-6 ">
         <h2 className='primary signup_text mx-auto p-10'>Signup to get started </h2>
@@ -104,12 +140,10 @@ function RegisterUser() {
       <MDBInput onChange={(e)=>{setpassword(e.target.value.trim())}} wrapperClass='mb-4' label='Password' id='form2' type='password'/>
       <MDBInput onChange={(e)=>{setconfirmPassword(e.target.value.trim())}} wrapperClass='mb-4' label='Confirm Password' id='form2' type='password'/>
 
-      <MDBBtn onClick={(e)=>handleRegisterClick(e)} className="mb-4 mx-auto col-lg-6 col-8" disabled={isRegisterBtnVisisble?"":"false"}>{registerBtnText}</MDBBtn>
+      { isRegisterBtnEnabled && <MDBBtn onClick={(e)=>handleRegisterClick(e)} className="mb-4 mx-auto col-lg-6 col-8" disabled={isRegisterBtnVisisble?"":"false"}>{registerBtnText}</MDBBtn>}
 
       <div className="text-center">
-        <p>didn't spent much on security , make sure to use strong password.
-            And dont forget your passowrd 
-        </p>
+       
 
         <div className='d-flex justify-content-between mx-auto' style={{width: '40%'}}>
           <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
@@ -117,11 +151,19 @@ function RegisterUser() {
           </MDBBtn>
         </div>
       </div>
+      
+      {IsAlertEnabled && <Alert type={alertType} message={AlertText} />}
+      {enableOTPTextbox && <MDBInput onChange={(e)=>{setOtp(e.target.value)}} wrapperClass='mb-4' label='enter otp' id='email_otp' type='text'/>} 
+      {enableOTPTextbox &&  <MDBBtn onClick={(e)=>{handleValidateOTPclick()}} className="mb-4 mx-auto col-lg-6 col-8" >validate otp</MDBBtn>} 
+
+
       {formValidationFailedMessage.trim() != '' && <div class="alert alert-danger" role="alert">
         {formValidationFailedMessage}
-        
       </div> }
-      {IsAlertEnabled && <Alert type={alertType} message={AlertText} />}
+
+      <p>didn't spent much on security , make sure to use strong password.
+            And dont forget your passowrd 
+        </p>
     </MDBContainer>
   );
 }
